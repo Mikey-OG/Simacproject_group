f672add559626a88d3c533c320bfac33823fa070
I407846 <o.aboudehn@student.fontys.nl>
2022-05-13 09:44:33 +0200

    exam creation frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import Examstable from './components/Examstable';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import CreateExamComponent from './components/CreateExam';
+import Home from './components/ExamFrom.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Home></Home>
+    // <div className="App">
+    //   <header className="App-header">
         
-        <p>
-            Exams Dashboard
-        </p>
+    //     <p>
+    //         Exams Dashboard
+    //     </p>
        
-        <Examstable></Examstable>   
-      </header>
-    </div>
+    //     <Examstable></Examstable>   
+    //   </header>
+    // </div>
     
   );
 }
